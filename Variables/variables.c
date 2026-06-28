8a3abb08e494341ff72276d0dd3cb737587367e6
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-06-28 15:21:14 +0530

    Add variables and print statements in variables.c

---
+++ Variables/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 23;
+    float cgpa = 7.6;
+    char grade = 'A';
+
+    printf("Age   : %d\n", age);
+    printf("CGPA  : %.1f\n", cgpa);
+    printf("Grade : %c\n", grade);
+
+    return 0;
+}
