c617f4cc4aaae606c539a6a4c68fe9a8b47746ff
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-06-29 20:56:43 +0530

    Add constants for PI and DAYS_IN_WEEK

---
+++ Variables/constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main()
+{
+    const int DAYS_IN_WEEK = 7;
+
+    printf("Value of PI           : %.5f\n", PI);
+    printf("Days in a Week        : %d\n", DAYS_IN_WEEK);
+
+    return 0;
+}
