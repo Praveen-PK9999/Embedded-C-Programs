e5f8096a916b2802336b90bbed270186e1a40a0b
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-06-29 21:02:19 +0530

    Add local variable example in 03_local_variables.c

---
+++ Variables/03_local_variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void display()
+{
+    int num = 10;   // Local Variable
+
+    printf("Inside display() : %d\n", num);
+}
+
+int main()
+{
+    display();
+
+    return 0;
+}
