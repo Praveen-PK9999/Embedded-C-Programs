8b16eab58a641a50b6fbcf9c054222ef2ee38918
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-02 21:15:26 +0530

    Add static variable example for counter function

---
+++ Variables/05_static_variable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void counter()
+{
+    static int count = 0;
+
+    count++;
+
+    printf("Counter = %d\n", count);
+}
+
+int main()
+{
+    counter();
+    counter();
+    counter();
+    counter();
+
+    return 0;
+}
