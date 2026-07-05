76e96357072e41b1e4d4b5d769212b7d33b90ebb
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-05 20:51:11 +0530

    Create 04_double.c

---
+++ Data_Types/04_double.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double pi = 3.141592653589793;
+    double distance = 123456.789123;
+    double salary = 45678.987654;
+
+    printf("Value of PI : %.15lf\n", pi);
+    printf("Distance    : %.6lf\n", distance);
+    printf("Salary      : %.6lf\n", salary);
+
+    return 0;
+}
