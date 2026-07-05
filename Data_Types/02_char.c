26b47bc966b2903a4067b02bd9e62b8b29cb6eac
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-05 20:43:07 +0530

    Add char data type example in 02_char.c

---
+++ Data_Types/02_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char grade = 'A';
+    char gender = 'M';
+    char symbol = '#';
+
+    printf("Grade  : %c\n", grade);
+    printf("Gender : %c\n", gender);
+    printf("Symbol : %c\n", symbol);
+
+    return 0;
+}
