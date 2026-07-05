0e8a4ebc12ffab4c7561edd8efc23d7a72355ddf
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-05 20:38:07 +0530

    Add int.c to demonstrate basic integer usage

---
+++ Data_Types/01_int.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 23;
+    int marks = 95;
+    int salary = 25000;
+
+    printf("Age    : %d\n", age);
+    printf("Marks  : %d\n", marks);
+    printf("Salary : %d\n", salary);
+
+    return 0;
+}
