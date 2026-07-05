72ad3bdea681375ea9879cd6ff812d2bbb2e0588
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-05 20:45:10 +0530

    Add float variable examples in 03_float.c

---
+++ Data_Types/03_float.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float temperature = 36.5;
+    float voltage = 5.0;
+    float cgpa = 7.60;
+
+    printf("Temperature : %.2f °C\n", temperature);
+    printf("Voltage     : %.2f V\n", voltage);
+    printf("CGPA        : %.2f\n", cgpa);
+
+    return 0;
+}
