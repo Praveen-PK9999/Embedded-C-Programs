30b287c7e4cce7cb0710fb6da97ca40ad0b0048f
K. Praveen Kumar <praveensumanthmanisha0@gmail.com>
2026-07-07 21:09:34 +0530

    Add arithmetic operators example in C

---
+++ Operators/01_arithmetic_operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 20, b = 10;
+
+    printf("First Number  : %d\n", a);
+    printf("Second Number : %d\n\n", b);
+
+    printf("Addition       : %d\n", a + b);
+    printf("Subtraction    : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division       : %d\n", a / b);
+    printf("Modulus        : %d\n", a % b);
+
+    return 0;
+}
